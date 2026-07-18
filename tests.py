bc9a26cf5126965624b34fa081dc81771df7b0ad
ptwt3d-creator <ptwt3d@gmail.com>
2026-07-18 17:09:44 +0300

    fix test asserts 'test_get_books_for_children_rait_book'

--- tests.py
+++ tests.py
@@ -84,7 +84,7 @@ class TestBooksCollector:
         assert book in collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
 
         collector.set_book_genre(book, 'Детективы')
-        assert collector.books_genre[book] in collector.genre , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
+        assert 'Детективы' in collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
         
         assert book not in collector.get_books_for_children()
 
