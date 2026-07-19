fa8fbda813fb2d34f2bb506dbe50bc481c67acde
ptwt3d-creator <ptwt3d@gmail.com>
2026-07-19 14:30:37 +0300

    add fixture 'collector_book' (autouse=True)

--- tests.py
+++ tests.py
@@ -1,129 +1,120 @@
 from main import BooksCollector
-
+import pytest
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
 
+    @pytest.fixture(autouse=True)
+    def collector_book(self):
+        self.collector = BooksCollector()
     # пример теста:
     # обязательно указывать префикс test_
     # дальше идет название метода, который тестируем add_new_book_
     # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
+        # создаем экземпляр (объект) класса Booksself.self.collector
         # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        self.collector.add_new_book('Гордость и предубеждение и зомби')
+        self.collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
         # словарь books_genre, который нам возвращает метод get_books_genre, имеет длину 2
-        assert len(collector.get_books_genre()) == 2
+        assert len(self.collector.get_books_genre()) == 2
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
     
     def test_set_genre_one_new_book(self):
-        collector = BooksCollector()
         genre = 'Комедии'
 
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-        collector.set_book_genre('Что делать, если ваш кот хочет вас убить', genre)
+        self.collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        self.collector.set_book_genre('Что делать, если ваш кот хочет вас убить', genre)
 
-        assert collector.get_book_genre('Что делать, если ваш кот хочет вас убить') == genre
+        assert self.collector.get_book_genre('Что делать, если ваш кот хочет вас убить') == genre
 
     def test_set_genre_not_ganre_list_for_book(self):
-        collector = BooksCollector()
         genre = 'Синий'
 
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-        collector.set_book_genre('Что делать, если ваш кот хочет вас убить', genre)
+        self.collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        self.collector.set_book_genre('Что делать, если ваш кот хочет вас убить', genre)
 
-        assert genre not in collector.get_book_genre('Что делать, если ваш кот хочет вас убить')
+        assert genre not in self.collector.get_book_genre('Что делать, если ваш кот хочет вас убить')
 
     def test_add_book_in_favorites_add_book(self):
-        collector = BooksCollector()
+        self.collector.add_new_book('Букварь')
+        self.collector.add_book_in_favorites('Букварь')
 
-        collector.add_new_book('Букварь')
-        collector.add_book_in_favorites('Букварь')
-
-        assert 'Букварь' in collector.favorites
+        assert 'Букварь' in self.collector.favorites
 
     def test_delete_book_from_favorites_add_and_del_book(self):
-        collector = BooksCollector()
         book = 'Букварь'
 
-        collector.add_new_book(book)
-        assert book in collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
+        self.collector.add_new_book(book)
+        assert book in self.collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
         
-        collector.add_book_in_favorites(book)
-        assert book in collector.favorites, 'Книга не добавилась в favorites - необходимо для продолжения теста'
+        self.collector.add_book_in_favorites(book)
+        assert book in self.collector.favorites, 'Книга не добавилась в favorites - необходимо для продолжения теста'
         
-        collector.delete_book_from_favorites(book)
-        assert book not in collector.favorites
+        self.collector.delete_book_from_favorites(book)
+        assert book not in self.collector.favorites
 
     def test_get_list_of_favorites_books_add_and_get_book(self):
-        collector = BooksCollector()
         book = 'Букварь2.0'
 
-        collector.add_new_book(book)
-        assert book in collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
+        self.collector.add_new_book(book)
+        assert book in self.collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
         
-        collector.add_book_in_favorites(book)
-        assert book in collector.favorites, 'Книга не добавилась в favorites - необходимо для продолжения теста'
+        self.collector.add_book_in_favorites(book)
+        assert book in self.collector.favorites, 'Книга не добавилась в favorites - необходимо для продолжения теста'
 
-        assert book in collector.get_list_of_favorites_books()
+        assert book in self.collector.get_list_of_favorites_books()
 
     def test_get_books_with_specific_genre_return_requested_book(self):
-        collector = BooksCollector()
         book = 'Дневник кота-убийцы'
 
-        collector.add_new_book(book)
-        assert book in collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
+        self.collector.add_new_book(book)
+        assert book in self.collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
 
-        collector.set_book_genre(book, 'Комедии')
-        assert 'Комедии' in collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
+        self.collector.set_book_genre(book, 'Комедии')
+        assert 'Комедии' in self.collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
         
-        assert book in collector.get_books_with_specific_genre('Комедии')
+        assert book in self.collector.get_books_with_specific_genre('Комедии')
 
     def test_get_books_with_specific_genre_not_return_not_requested_book(self):
-        collector = BooksCollector()
         book = 'Дневник кота-убийцы'
         book1 = 'оно'
 
-        collector.add_new_book(book)
-        collector.add_new_book(book1)
-        assert book in collector.books_genre, 'Книга1 не добавилась в books_genre - необходимо для продолжения теста'
-        assert book1 in collector.books_genre, 'Книга2 не добавилась в books_genre - необходимо для продолжения теста'
+        self.collector.add_new_book(book)
+        self.collector.add_new_book(book1)
+        assert book in self.collector.books_genre, 'Книга1 не добавилась в books_genre - необходимо для продолжения теста'
+        assert book1 in self.collector.books_genre, 'Книга2 не добавилась в books_genre - необходимо для продолжения теста'
         
-        collector.set_book_genre(book, 'Комедии')
-        collector.set_book_genre(book1, 'Ужасы')
-        assert 'Комедии' in collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
-        assert 'Ужасы' in collector.books_genre[book1] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
+        self.collector.set_book_genre(book, 'Комедии')
+        self.collector.set_book_genre(book1, 'Ужасы')
+        assert 'Комедии' in self.collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
+        assert 'Ужасы' in self.collector.books_genre[book1] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
 
-        assert book in collector.get_books_with_specific_genre('Комедии'), 'Тест прерван, метод не отработал вывод книг по запрошенному жанру - необходимо для продолжения теста'
-        assert book1 not in collector.get_books_with_specific_genre('Комедии')
+        assert book in self.collector.get_books_with_specific_genre('Комедии'), 'Тест прерван, метод не отработал вывод книг по запрошенному жанру - необходимо для продолжения теста'
+        assert book1 not in self.collector.get_books_with_specific_genre('Комедии')
 
     def test_get_books_for_children_rait_book(self):
-        collector = BooksCollector()
         book = 'Бойцовский клуб'
 
-        collector.add_new_book(book)
-        assert book in collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
+        self.collector.add_new_book(book)
+        assert book in self.collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
 
-        collector.set_book_genre(book, 'Детективы')
-        assert 'Детективы' in collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
+        self.collector.set_book_genre(book, 'Детективы')
+        assert 'Детективы' in self.collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
         
-        assert book not in collector.get_books_for_children()
+        assert book not in self.collector.get_books_for_children()
 
     def test_get_books_for_children_not_rait_book(self):
-        collector = BooksCollector()
         book = 'Винни-Пух'
 
-        collector.add_new_book(book)
-        assert book in collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
+        self.collector.add_new_book(book)
+        assert book in self.collector.books_genre, 'Книга не добавилась в books_genre - необходимо для продолжения теста'
 
-        collector.set_book_genre(book, 'Комедии')
-        assert 'Комедии' in collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
+        self.collector.set_book_genre(book, 'Комедии')
+        assert 'Комедии' in self.collector.books_genre[book] , 'Книге в books_genre не добавился genre - необходимо для продолжения теста'
         
-        assert book in collector.get_books_for_children()
+        assert book in self.collector.get_books_for_children()
